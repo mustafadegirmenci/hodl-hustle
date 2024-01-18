41e600fe96681704e338b2f165703f5ccc26435e
mustafadegirmenci <m.dgr@yahoo.com>
2024-01-18 15:28:51 +0300

    Initial commit

---
+++ Assets/Modules/CameraSystem/CameraController.cs
@@ -0,0 +1,87 @@
+﻿using Cinemachine;
+using DG.Tweening;
+using SunkCost.HH.Modules.InputSystem;
+using UnityEngine;
+
+namespace SunkCost.HH.Modules.CameraSystem
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform cameraTarget;
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
+        [SerializeField] private float movementSpeed = 10f;
+        [SerializeField] private float rotationSpeed = 60f;
+        [SerializeField] private float zoomSpeed = 1f;
+
+        private Vector2 _moveInput;
+        private bool _rotateClockwiseInput;
+        private bool _rotateCounterClockwiseInput;
+        private Tween _zoomTween;
+        
+        private CinemachineTransposer _transposer;
+
+        private void Awake()
+        {
+            _transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        }
+
+        private void OnEnable()
+        {
+            InputManager.instance.onMoveInput.AddListener(m => _moveInput = m);
+            InputManager.instance.onRotateClockwiseInput.AddListener(r => _rotateClockwiseInput = r);
+            InputManager.instance.onRotateCounterClockwiseInput.AddListener(r => _rotateCounterClockwiseInput = r);
+            InputManager.instance.onZoomInput.AddListener(zoomAmount =>
+            {
+                var targetOffset = new Vector3(
+                    _transposer.m_FollowOffset.x,
+                    Mathf.Clamp(_transposer.m_FollowOffset.y - zoomAmount * zoomSpeed, 3, 12),
+                    Mathf.Clamp(_transposer.m_FollowOffset.z + zoomAmount * zoomSpeed, -12, -3)
+                );
+                
+                _zoomTween = DOTween.To(() => _transposer.m_FollowOffset, x => _transposer.m_FollowOffset = x, targetOffset, 0.5f);
+            });
+        }
+
+        private void OnDisable()
+        {
+            InputManager.instance.onMoveInput.RemoveAllListeners();
+            InputManager.instance.onRotateClockwiseInput.RemoveAllListeners();
+            InputManager.instance.onRotateCounterClockwiseInput.RemoveAllListeners();
+            InputManager.instance.onZoomInput.RemoveAllListeners();
+        }
+
+        private void Update()
+        {
+            var right = cameraTarget.right;
+            var position = cameraTarget.position;
+            var rotation = cameraTarget.rotation;
+            var rotationEuler = rotation.eulerAngles;
+            
+            var moveX = right * (_moveInput.x * movementSpeed);
+            var moveY = Vector3.Cross(right, Vector3.up) * (_moveInput.y * movementSpeed);
+            var targetPosition = position + moveX + moveY;
+            
+            cameraTarget.position = Vector3.Lerp(position, targetPosition, Time.deltaTime);
+
+            if (_rotateClockwiseInput)
+            {
+                var targetRotation = Quaternion.Euler(
+                    rotationEuler.x,
+                    rotationEuler.y + rotationSpeed * Time.deltaTime,
+                    rotationEuler.z
+                );
+                cameraTarget.rotation = Quaternion.Slerp(rotation, targetRotation, Time.deltaTime);
+            }
+
+            if (_rotateCounterClockwiseInput)
+            {
+                var targetRotation = Quaternion.Euler(
+                    rotationEuler.x,
+                    rotationEuler.y - rotationSpeed,
+                    rotationEuler.z
+                );
+                cameraTarget.rotation = Quaternion.Slerp(rotation, targetRotation, Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Modules/InputSystem/InputManager.cs
@@ -0,0 +1,83 @@
+﻿using Modules.InputSystem;
+using SunkCost.HH.Modules.Utility;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace SunkCost.HH.Modules.InputSystem
+{
+    public class InputManager : MonoSingleton<InputManager>, Controls.ICameraActions
+    {
+        [HideInInspector] public UnityEvent<Vector2> onMoveInput = new();
+        [HideInInspector] public UnityEvent<bool> onRotateClockwiseInput = new();
+        [HideInInspector] public UnityEvent<bool> onRotateCounterClockwiseInput = new();
+        [HideInInspector] public UnityEvent<float> onZoomInput = new();
+        
+        private Controls _controls;
+
+        #region MonoBehaviour Methods
+        
+        private void OnEnable()
+        {
+            if (_controls != null)
+            {
+                return;
+            }
+
+            _controls = new Controls();
+            _controls.Camera.SetCallbacks(this);
+            _controls.Camera.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _controls.Camera.Disable();
+        }
+        
+        #endregion
+
+        #region Callback Methods
+        
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            if (context.performed || context.canceled)
+            {
+                onMoveInput.Invoke(context.ReadValue<Vector2>());
+            }
+        }
+
+        public void OnRotateClockwise(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                onRotateClockwiseInput.Invoke(true);
+            }
+            else if (context.canceled)
+            {
+                onRotateClockwiseInput.Invoke(false);
+            }
+        }
+
+        public void OnRotateCounterClockwise(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                onRotateCounterClockwiseInput.Invoke(true);
+            }
+            else if (context.canceled)
+            {
+                onRotateCounterClockwiseInput.Invoke(false);
+            }
+        }
+
+        public void OnZoom(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                onZoomInput.Invoke(context.ReadValue<float>());
+            }
+        }
+
+        #endregion
+    }
+}
